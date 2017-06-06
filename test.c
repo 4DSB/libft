70f911f695efdd3de5a6d65ff690b97fc9a6bb86
Anurag Mittal <anurag.213@gmail.com>
2017-06-05 23:09:02 -0700

    completed bzero

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: amittal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/05 12:19:13 by amittal           #+#    #+#             */
-/*   Updated: 2017/06/05 21:33:18 by amittal          ###   ########.fr       */
+/*   Updated: 2017/06/05 22:57:03 by amittal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,15 @@ int main()
 	ft_memset(ch1, '-', 6);
 	print_bytes(ch1, 15);
 	printf("%s\n", ch1);
+	ps();
+
+	ps();
+	char test[] = "ft_bzero test";
+	printf("%s\n", test);
+	print_bytes(test, 13);
+	ft_bzero(test, 5);
+	print_bytes(test, 13);
+	printf("%s\n", test);
 	ps();
 }
 
